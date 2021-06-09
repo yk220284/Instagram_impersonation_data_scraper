082c1ce8c3964e16c05c393775b8def7c77dab24
Yankang <yankang.zhu@icloud.com>
2021-06-09 18:25:29 +0100

    test extracting post id

---
+++ test/test_json_processor.py
@@ -0,0 +1,13 @@
+import json
+from unittest import TestCase
+
+from json_processor import collect_values
+
+
+class Test(TestCase):
+    def test_collect_values(self):
+        with open('fakehashtag.json') as f:
+            json_dict = json.load(f)
+            rlt = collect_values(json_dict, 'code', 'a')
+            self.assertEqual(len(rlt['code']), 39)
+            self.assertEqual(len(rlt['a']), 0)
