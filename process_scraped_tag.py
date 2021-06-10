40441b953db4e1bfed6693648b3549ffb92b984b
Yankang <yankang.zhu@icloud.com>
2021-06-10 17:37:19 +0100

    use threading to download post pictures

--- process_scraped_tag.py
+++ process_scraped_tag.py
@@ -1,5 +1,32 @@
-import pandas as pd
+import concurrent.futures
+import time
 
-df = pd.read_csv("fake_account_posts.csv")
+import pandas as pd
+from instascrape import Post
+
+from post_adaptor import PostAdaptor
+
+df = pd.read_csv("data/fake_account_posts.csv")
 print(df.size)
 print(df['code'].unique().size)
+
+t1 = time.perf_counter()
+
+
+def download_image(post_code):
+    print(f"saving {post_code}")
+    post = PostAdaptor(Post(post_code))
+    post.save_media(f"data/img/{post_code}.png")
+    print(f"post {post_code} saved...")
+
+
+codes = df['code'].unique()[:30]
+# for code in codes:
+#     download_image(code)
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    executor.map(download_image, codes)
+
+t2 = time.perf_counter()
+
+print(f'Finished in {t2 - t1} seconds')
