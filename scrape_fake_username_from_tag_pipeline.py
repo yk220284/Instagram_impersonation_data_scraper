fa4d44ccb7b5e2c65651486f68e6f18fdc174c74
Yankang <yankang.zhu@icloud.com>
2021-06-15 21:33:29 +0100

    move TagScraper

--- scrape_fake_username_from_tag_pipeline.py
+++ scrape_fake_username_from_tag_pipeline.py
@@ -1,7 +1,7 @@
 import os
 
+from instagram_scraper.tag_scraper import TagScraper
 from scrape_posts_under_tag import scrape_posts
-from scrape_tag import TagScraper
 from text_extraction import find_similar_names_from_posts
 
 DATA_DIR = 'data/0615'
@@ -14,8 +14,8 @@ POST___JSON = os.path.join(DATA_DIR, 'post.json')
 IMG_DIR = os.path.join(DATA_DIR, 'img')
 PROCESSED_POST___JSON = os.path.join(DATA_DIR, 'processed_post.json')
 
-# fake_account_tag = TagScraper('fakeaccount', MAX_ID_FILE)
-# fake_account_tag.scrape_pages(page_cnt=200, sleep_interval=2)
-# fake_account_tag.save_record(POST_CODE___CSV, MAX_ID_FILE)
-# scrape_posts(POST_CODE___CSV, POST___JSON, IMG_DIR)
+fake_account_tag = TagScraper('fakeaccount', MAX_ID_FILE)
+fake_account_tag.scrape_pages(page_cnt=200, sleep_interval=2)
+fake_account_tag.save_record(POST_CODE___CSV, MAX_ID_FILE)
+scrape_posts(POST_CODE___CSV, POST___JSON, IMG_DIR)
 find_similar_names_from_posts(POST___JSON, IMG_DIR, PROCESSED_POST___JSON)
