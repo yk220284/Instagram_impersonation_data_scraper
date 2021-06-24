c96d8ce00b1faa6b7e0f86d2dfd485ae61be951f
Yankang <yankang.zhu@icloud.com>
2021-06-24 13:07:22 +0100

    find_close_word return success bool

--- text_extraction.py
+++ text_extraction.py
@@ -1,6 +1,6 @@
 import concurrent.futures
 import heapq
-from typing import List, Dict
+from typing import List, Dict, Tuple
 
 import pytesseract
 import textdistance
@@ -13,7 +13,11 @@ from username_policy import UserName
 
 class ImageTextExtractor:
     def __init__(self, img_path: str, rule=None):
-        self.img = Image.open(img_path)
+        try:
+            self.img = Image.open(img_path)
+        except Exception as err:
+            print(f"err opening image: {err}")
+            self.img = None
         self.words = None
         self.rule = rule
 
@@ -27,15 +31,19 @@ class ImageTextExtractor:
         return self.words
 
     def find_closest_words(self, target: str, top_k: int, distance_threshold: float = float('inf')) \
-            -> Dict[str, List[dict]]:
+            -> Tuple[Dict[str, List[dict]], bool]:
         """
 
         :param distance_threshold:
         :param target: word to compare to
         :param top_k: most similar k words
-        :return: list of most similar words of size top_k, element in the format of
+        :returns
+        processed_dict: list of most similar words of size top_k, element in the format of
         {target word: [{extracted similar word from image: distance}] }
+        success: bool if request succeeds
         """
+        if not isinstance(target, str) or self.img is None:
+            return {target: []}, False
         rlt = []
         for word in self.extract_words():
             d = textdistance.levenshtein(target, word)
@@ -44,27 +52,23 @@ class ImageTextExtractor:
                 if len(rlt) > top_k:
                     heapq.heappop(rlt)
         # format
-        return {target: [{word: distance} for distance, word in rlt]}
+        return {target: [{word: distance} for distance, word in rlt]}, bool(rlt)
 
 
-def extract_fake_username(json_dict, img_dir, top_k=2, distance_threshold=4):
+def extract_fake_username(json_dict, img_dir, top_k=2):
     username = json_dict['username']
-    if not isinstance(username, str):
-        # NaN username 
-        return None
-    print(f"processing username: {json_dict['username']}, code: {json_dict['shortcode']}")
     img_path = f"{img_dir}/{json_dict['shortcode']}.png"
-    try:
-        image = ImageTextExtractor(img_path, UserName.is_username)
-    except Exception as err:
-        print(err)
-        return None
-    rlt = image.find_closest_words(username, top_k, len(username) // 2)
-    if rlt[username]:
-        # image contain words
+    print(f"processing username: {json_dict['username']}, code: {json_dict['shortcode']}")
+
+    image = ImageTextExtractor(img_path, UserName.is_username)
+
+    # set distance threshold
+    distance_threshold = len(username) // 2 if isinstance(username, str) else float('inf')
+
+    rlt, success = image.find_closest_words(username, top_k, distance_threshold)
+    if success:
         json_dict['fake_names'] = rlt[username]
-        return json_dict
-    return None
+    return json_dict, success
 
 
 @timing
@@ -75,10 +79,5 @@ def find_similar_names_from_posts(post_json_file, img_dir, rlt_file):
     with concurrent.futures.ThreadPoolExecutor() as executor:
         args = [(json_dict, img_dir) for json_dict in unsaved_json_dicts]
         results = executor.map(lambda p: extract_fake_username(*p), args)
-        rlt = [result for result in results if result]
-        print(f"Found {len(rlt)} pair")
-        JsonDict.extend(rlt, rlt_file)
-
-
-if __name__ == '__main__':
-    find_similar_names_from_posts("data/post_0615.json", "data/img/0615", "data/fake_username_0615.json")
+        rlt = [r for r, success in results if success]
+        JsonDict.save(rlt, rlt_file)
